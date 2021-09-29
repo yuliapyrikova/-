4d85e6ae217a217dd849e2ae93a92a46de8e6170
yuliapyrikova <pyrikova.yulia@mail.ru>
2021-09-30 02:53:49 +0300

    30.09.21

---
+++ Controllers/GymController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace pyrikova.Controllers
+{
+            [ApiController]
+            [Route("[/visitor]")]
+            public class VisitorController : ControllerBase
+            {
+            [HttpGet]
+            [Route("/visitor/MonitorsTheSubscription")]
+
+            public string MonitorsTheSubscription(string str)
+            {
+            return "Метод наблюдения за абонементом";
+            }
+
+            [HttpGet]
+            [Route("/visitor/Record")]
+
+            public string Record(string str)
+            {
+            return "Метод записи на тренировку";
+            }
+
+            [HttpGet]
+            [Route("/visitor/Feedback")]
+
+            public string Feedback(string str)
+            {
+            return "Метод написания отзыва";
+             }
+  
+
+            [ApiController]
+            [Route("[/coach]")]
+            public class CoachController : ControllerBase
+            {
+            [HttpPut]
+            [Route("/coach/FollowTheSchedule")]
+
+            public string FollowTheSchedule(string str)
+            {
+                return "Метод наблюдения за собственным расписанием";
+            }
+
+            [HttpPut]
+            [Route("/coach/VisitorInformation")]
+
+            public string VisitorInformation(string str)
+            {
+                return "Метод просматривания информации о записавшемся";
+            }
+
+
+            [ApiController]
+            [Route("[/admin]")]
+            public class AdminController : ControllerBase
+            {
+            [HttpPost]
+            [Route("/admin/Extradite")]
+
+            public string Extradite(string str)
+            {
+                    return "Метод выдачи абонемента";
+            }
+
+            [HttpPost]
+            [Route("/admin/Extension")]
+
+            public string Extension(string str)
+            {
+                    return "Метод продления абонемента";
+            }
+
+            [HttpPost]
+            [Route("/admin/Scheduling")]
+
+            public string Scheduling(string str)
+            {
+                    return "Метод составления расписания тренировок";
+            }
+
+            [HttpPost]
+            [Route("/admin/Request")]
+
+            public string Request(string str)
+            {
+                    return "Метод запроса отзыва";
+            }
+
+            [HttpPost]
+            [Route("/admin/NewsFeed")]
+
+            public string NewsFeed(string str)
+            {
+                    return "Метод ведения ленты новостей";
+            }
+
+            }
+        }
+    }
+}
