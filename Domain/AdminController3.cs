8ebeefe9cbed00f1f379ffa496ec8bf00891b9c0
Dmitry Anikeev <anikdv@gmail.com>
2021-11-11 11:22:34 +0300

    Update AdminController3.cs

--- Domain/AdminController3.cs
+++ Domain/AdminController3.cs
@@ -1,6 +1,7 @@
 ﻿class Admin
 {
     public string ScheduleCode { get; set; }
-    public string News { get; set; }
+    public int News { get; set; }
+    public News News { get; set; }
     public string Subscription { get; set; }
 }
